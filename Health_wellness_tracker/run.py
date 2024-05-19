4ae30f99a784600f06932ba9dee0bc52548459e8
Ephyz8 <ziwoyaephraim@gmail.com>
2024-05-19 23:59:29 +0200

    done

--- Health_wellness_tracker/run.py
+++ Health_wellness_tracker/run.py
@@ -1,7 +1,9 @@
 import os
+from app import create_app
+
+app = create_app('prod')
 
 
 if __name__ == "__main__":
-    from app import create_app
-    app = create_app('prod')
+    
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
