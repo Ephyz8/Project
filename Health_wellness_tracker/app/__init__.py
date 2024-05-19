88d0754354242a20f098dec601135742405f37cc
Ephyz8 <ziwoyaephraim@gmail.com>
2024-05-19 22:33:45 +0200

    done

--- Health_wellness_tracker/app/__init__.py
+++ Health_wellness_tracker/app/__init__.py
@@ -2,16 +2,18 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-login_manager = LoginManager()
+from config import config_by_name
 
 db = SQLAlchemy()
 migrate = Migrate()
+login_manager = LoginManager()
 
 
 def create_app():
     """Creates and configures an instance of the Flask application.
     """
     app = Flask(__name__)
+    app.config.from_object(config_by_name[config_name])
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///health_tracker.db'
     app.config['SQLACHEMY_TRACK_MODIFCATIONS'] = False
     app.config['SECRET_KEY'] = 'your_secret_key'
---
+++ config.py
@@ -0,0 +1,29 @@
+import os
+
+class Config:
+    """Base configuration."""
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'your_default_secret_key')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///site.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    DEBUG = False
+
+class DevelopmentConfig(Config):
+    """Development configuration."""
+    DEBUG = True
+
+class TestingConfig(Config):
+    """Testing configuration."""
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
+    DEBUG = True
+
+class ProductionConfig(Config):
+    """Production configuration."""
+    DEBUG = False
+
+# Dictionary to map the environment name to the config class
+config_by_name = dict(
+    dev=DevelopmentConfig,
+    test=TestingConfig,
+    prod=ProductionConfig
+)
