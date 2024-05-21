a6640a7e8ac8d739de6357206c3d4614e6bce6b4
Ephyz8 <ziwoyaephraim@gmail.com>
2024-05-21 05:21:17 +0200

    configurations

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from flask_login import UserMixin
 from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class User(UserMixin, db.Model):
@@ -11,6 +12,13 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(128), nullable=False)
     activities = db.relationship('Activity', backref='user', lazy=True)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
 
     def __repr__(self):
        """
