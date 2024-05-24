909d35963fb5d25a9b71e4d9ca7d6c368188a36b
Ephyz8 <ziwoyaephraim@gmail.com>
2024-05-24 19:30:43 +0200

    frontend

--- create_db.py
+++ create_db.py
@@ -1,6 +1,8 @@
 from app import create_app, db
 
+# Replace development with the appropriate configuration name
 app = create_app('development')
 
 with app.app_context():
     db.create_all()
+    print("Database tables created successfully.")
